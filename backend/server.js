f75fc4bbc086ab23de934e5297d310f9cd71c35a
Inna Kukla <0969125133@ukr.net>
2025-03-26 22:02:53 +0200

    Fixed render

--- backend/server.js
+++ backend/server.js
@@ -42,4 +42,4 @@ app.use("/api/products", productsRoutes);
 
 // ✅ Запуск сервера
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, " 0.0.0.0", () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
