f01b30721d01a1e7139048204687172374a849ca
Inna Kukla <0969125133@ukr.net>
2025-07-31 01:34:01 +0300

    edit server cors arrow allowed

--- backend/server.js
+++ backend/server.js
@@ -48,7 +48,7 @@ app.use(
   cors({
     origin: ["https://innakukla.github.io"],
     methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
+    allowedHeaders: ["Content-Type", "Authorization"],
     // credentials: true
   })
 );
