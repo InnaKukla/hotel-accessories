c90e8b1ad06fe259e1b2035dada413d4448a0213
Inna Kukla <0969125133@ukr.net>
2025-08-01 00:08:35 +0300

    update basename

--- src/index.jsx
+++ src/index.jsx
@@ -12,7 +12,7 @@ root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <StrictMode>
-        <BrowserRouter basename="/hotel-accessories">
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </StrictMode>
